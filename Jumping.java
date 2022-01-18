7244ecf0626a28a86174f71ccc53ab3c72e119b0
Alexandr <95639393+Duhan8alex@users.noreply.github.com>
2022-01-18 20:52:22 +0700

    Add files via upload

---
+++ Jumping.java
@@ -0,0 +1,13 @@
+
+
+public class Jumping extends Obstacle {
+
+    public Jumping(int difficulty) {
+        super(difficulty);
+    }
+
+    @Override
+    public void goChallenge(TeamMember member) {
+        member.jump(super.getDifficulty());
+    }
+}
